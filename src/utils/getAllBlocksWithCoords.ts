e2721f9efc77e99810d882c0c79be8202bff6f26
hkgnp <ben@pngs.cc>
2022-11-20 16:09:05 +0800

    added recursion support to find coords blocks in child blocks

--- src/utils/getAllBlocksWithCoords.ts
+++ src/utils/getAllBlocksWithCoords.ts
@@ -1,22 +1,37 @@
+import { BlockEntity } from "@logseq/libs/dist/LSPlugin.user";
+
 export default async function getAllBlocksWithCoords(uuid: string) {
   const blk = await logseq.Editor.getBlock(uuid);
-  if (blk) {
-    const page = await logseq.Editor.getPage(blk.page.id);
-    if (page) {
-      const pbt = await logseq.Editor.getPageBlocksTree(page.name);
-      // Filters out blocks with coords
-      return pbt
-        .filter((b) => b.properties!.coords)
-        .map((b) => ({
-          uuid: b.uuid,
-          content: b.content.split("\ncoords")[0],
-          coords: b
-            .properties!.coords.replaceAll("°", "")
+  let allCoordsBlocksOnPage: any[] = [];
+
+  function recursiveFind(pbt: BlockEntity[]) {
+    for (let i = 0; i < pbt.length; i++) {
+      if (pbt[i].properties!.coords) {
+        allCoordsBlocksOnPage.push({
+          uuid: pbt[i].uuid,
+          content: pbt[i].content.split("\ncoords")[0],
+          coords: pbt[i]
+            .properties!.coords.replaceAll("\u00B0", "")
             .replaceAll("N", "")
             .replaceAll("E", "")
             .trim()
             .split(","),
-        }));
+        });
+      }
+      if (pbt[i].children) {
+        recursiveFind(pbt[i].children as BlockEntity[]);
+      } else {
+        break;
+      }
     }
   }
+
+  if (blk) {
+    const page = await logseq.Editor.getPage(blk.page.id);
+    if (page) {
+      const pbt = await logseq.Editor.getPageBlocksTree(page.name);
+      recursiveFind(pbt);
+    }
+  }
+  return allCoordsBlocksOnPage;
 }
