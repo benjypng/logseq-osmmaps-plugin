f59f6bf26d0e1e14133706d82fbf41a5eb37eba3
hkgnp <hkgnp@github.com>
2022-12-29 15:26:25 +0800

    added var slot

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,44 @@
-import '@logseq/libs';
-import generateUniqueId from './utils/uniqueId';
-import callSettings from './utils/callSettings';
-import getMapCentrePoint from './getMapCentrePoint';
-import addElement from './utils/addElementToDom';
+import "@logseq/libs";
+import generateUniqueId from "./utils/uniqueId";
+import callSettings from "./utils/callSettings";
+import getMapCentrePoint from "./getMapCentrePoint";
+import addElement from "./utils/addElementToDom";
 
 function main() {
-  console.log('logseq-maps-plugin loaded');
+  console.log("logseq-maps-plugin loaded");
 
   callSettings();
 
   // Add Leaflet CSS
-  addElement('css', 'https://unpkg.com/leaflet@1.9.3/dist/leaflet.css');
+  addElement("css", "https://unpkg.com/leaflet@1.9.3/dist/leaflet.css");
   addElement(
-    'css',
-    'https://unpkg.com/leaflet-routing-machine@3.2.12/dist/leaflet-routing-machine.css'
+    "css",
+    "https://unpkg.com/leaflet-routing-machine@3.2.12/dist/leaflet-routing-machine.css"
   );
 
   // Add Leaflet JS
-  addElement('script', 'https://unpkg.com/leaflet@1.9.3/dist/leaflet.js');
+  addElement("script", "https://unpkg.com/leaflet@1.9.3/dist/leaflet.js");
   addElement(
-    'script',
-    'https://unpkg.com/leaflet-routing-machine@3.2.12/dist/leaflet-routing-machine.js'
+    "script",
+    "https://unpkg.com/leaflet-routing-machine@3.2.12/dist/leaflet-routing-machine.js"
   );
 
-  logseq.Editor.registerSlashCommand('add map', async () => {
+  logseq.Editor.registerSlashCommand("add map", async () => {
     const id = generateUniqueId();
     await logseq.Editor.insertAtEditingCursor(
       `{{renderer :map_${id}, default, Singapore}} [:div {:is "map-${id}"}]`
     );
   });
 
-  logseq.App.onMacroRendererSlotted(async ({ payload }) => {
+  logseq.App.onMacroRendererSlotted(async ({ payload, slot }) => {
     const [type, mapType, var1, var2] = payload.arguments;
     const uuid = payload.uuid;
-    if (!type.startsWith(':map_')) return;
-    const id = type.split('_')[1]?.trim();
+    if (!type.startsWith(":map_")) return;
+    const id = type.split("_")[1]?.trim();
+    const mapId = `map_${id}_${slot}`;
 
-    if (type.startsWith(':map_')) {
-      getMapCentrePoint(uuid, id, mapType, var1, var2);
+    if (type.startsWith(":map_")) {
+      getMapCentrePoint(uuid, mapId, mapType, var1, var2);
     }
   });
 }
