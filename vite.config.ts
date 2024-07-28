5125be30b7e948790db59912c1733ba5db4028a7
Benjy <ben@pngs.cc>
2024-07-28 16:20:39 +0800

    fix: build

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,9 @@ import { defineConfig } from 'vite'
 import logseqDevPlugin from 'vite-plugin-logseq'
 
 export default defineConfig(async () => {
-  const { viteStaticCopy } = await import('vite-plugin-static-copy')
+  //const { viteStaticCopy } = await import('vite-plugin-static-copy')
 
   return {
-    plugins: [
-      logseqDevPlugin(),
-      viteStaticCopy({
-        targets: [
-          {
-            src: 'leaflet/**/*',
-            dest: 'leaflet',
-          },
-        ],
-      }),
-    ],
+    plugins: [logseqDevPlugin()],
   }
 })
