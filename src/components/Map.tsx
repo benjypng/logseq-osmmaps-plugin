e32781c858450b502a0cff8c0d2396f7a02d9512
Benjy <ben@pngs.cc>
2024-07-28 15:04:12 +0800

    fix: css

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,11 +1,9 @@
 import '@logseq/libs'
 import '../../leaflet/leaflet.css'
 
-import { createControlComponent } from '@react-leaflet/core'
 import { LatLngTuple, Marker as LeafletMarker } from 'leaflet'
 import { useEffect, useRef, useState } from 'react'
-import { createRoot } from 'react-dom/client'
-import { MapContainer, Marker, Popup, useMap } from 'react-leaflet'
+import { MapContainer, Marker, Popup } from 'react-leaflet'
 
 import { LocationProps } from '../utils/get-locations-from-page'
 import { svgIcon } from '../utils/handle-icon'
@@ -55,24 +53,6 @@ const Map = ({
     return <strong>Loading Leaflet...</strong>
   }
 
-  const CreateCustomMapControl = (props: any) => {
-    const CustomControl = host.L.Control.extend({
-      onAdd: function () {
-        const container = host.L.DomUtil.create('div', 'custom-control')
-        host.L.DomEvent.disableClickPropagation(container)
-        host.L.DomEvent.disableScrollPropagation(container)
-        const root = createRoot(container)
-        root.render(<MapControl {...props} />)
-        return container
-      },
-      onRemove: function () {
-        // Cleanup
-      },
-    })
-    return new CustomControl({ position: 'bottomleft' })
-  }
-  const MapControlComponent = createControlComponent(CreateCustomMapControl)
-
   return (
     <>
       <MapContainer
@@ -97,13 +77,13 @@ const Map = ({
         <RightClickAddMarker uuid={uuid} setLocations={setLocations} />
         <FitBounds locations={locations} />
         <SetViewOnClick />
-        <MapControlComponent
-          setMapOption={setMapOption}
-          markersRef={markersRef}
-          setLocations={setLocations}
-          uuid={uuid}
-        />
       </MapContainer>
+      <MapControl
+        setMapOption={setMapOption}
+        markersRef={markersRef}
+        setLocations={setLocations}
+        uuid={uuid}
+      />
     </>
   )
 }
